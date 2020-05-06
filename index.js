99d8d964d1cc28c62bf987645d928dc922af35a1
Prabhakaran S <prabhakaran@zohocorp.com>
2020-05-06 09:54:57 +0530

    - simple http & socker server impl

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-var static = require('node-static');
+
+// Required Modules
+var express = require('express');
 var http = require('http');
+var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+var io = require('socket.io')(http);
 
-var file = new (static.Server)();
-var app = http.createServer(function (req, res) {
-    file.serve(req, res);
-}).listen(8181);
+// Create Express App
+var app = express();
+app.set('port', 8181);
 
-var io = require('socket.io').listen(app);
+app.use(logger('dev'));
 
-// Let's start managing connections...
+app.use(express.static('public'));
+app.use("/static", express.static('node_modules'));
+
+var server = http.createServer(app);
+io.listen(server);
+
 io.sockets.on('connection', function (socket) {
+  socket.on('messageChange', function (data) {
+    console.log(data);
+    socket.emit('receive', data.message.split('').reverse().join('') );
+  });
 
-    socket.on('message', function (message) {
-        log('S --> Got message: ', message);
-        socket.broadcast.to(message.channel).emit('message', message.message);
-    });
+  socket.on('message', function (message) {
+    log('S --> Got message: ', message);
+     socket.broadcast.to(message.channel).emit('message', message.message);
+   });
 
-    socket.on('create or join', function (channel) {
-        var numClients = io.sockets.clients(channel).length;
-        console.log('numclients = ' + numClients);
+  socket.on('create', function (channel) {
+     var numClients = io.sockets.clients(channel).length;
+     console.log('numclients = ' + numClients);
 
-        if (numClients == 0) {
-            socket.join(channel);
-            socket.emit('created', channel);
-        } else if (numClients == 1) {
-            io.sockets.in(channel).emit('remotePeerJoining', channel);
-            socket.join(channel);
-            socket.broadcast.to(channel).emit('broadcast: joined', 'S --> broadcast(): client ' + socket.id + ' joined channel ' + channel);
-        } else {
-            console.log("Channel full!");
-            socket.emit('full', channel);
-        }
-    });
+     if (numClients == 0) {
+         socket.join(channel);
+         socket.emit('created', channel);
+       } else if (numClients == 1) {
+           io.sockets.in(channel).emit('remotePeerJoining', channel);
+           socket.join(channel);
+           socket.broadcast.to(channel).emit('broadcast: joined', 'S --> broadcast(): client ' + socket.id + ' joined channel ' + channel);
+         } else {
+             console.log("Channel full!");
+             socket.emit('full', channel);
+           }
+   });
 
-    socket.on('response', function (response) {
-        log('S --> Got response: ', response);
-        socket.broadcast.to(response.channel).emit('response', response.message);
-    });
+   socket.on('response', function (response) {
+       log('S --> Got response: ', response);
+       socket.broadcast.to(response.channel).emit('response', response.message);
+     });
 
-    socket.on('Bye', function (channel) {
-        socket.broadcast.to(channel).emit('Bye');
-        socket.disconnect();
-    });
+  socket.on('Bye', function (channel) {
+    socket.broadcast.to(channel).emit('Bye');
+    socket.disconnect();
+  });
 
-    socket.on('Ack', function () {
-        console.log('Got an Ack!');
-        socket.disconnect();
-    });
-
-    function log() {
-        var array = [">>> "];
-        for (var i = 0; i < arguments.length; i++) {
-            array.push(arguments[i]);
-        }
-        socket.emit('log', array);
-    }
+  socket.on('Ack', function () {
+    console.log('Got an Ack!');
+    socket.disconnect();
+  });
 });
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
+
