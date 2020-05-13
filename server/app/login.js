60f6ab5a28ba2098c4e6a362b62cb93aa2073932
vigneshrajkumar <vigneshrajkumar@hotmail.com>
2020-05-13 15:18:11 +0530

    code cleanup

--- server/app/login.js
+++
@@ -1,8 +0,0 @@
-
-exports.register = async function(req, resp){
-    resp.send("register router called")
-}
-
-exports.login = async function(req, resp){
-    resp.send("signin router called")
-}
--- server/route.v1.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = function(app) {
-    app.get("/register", require("./app/login").register)
-}
