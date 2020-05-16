f4eba58ed5dfc78e9052ba24c1551378eb202929
vigneshrajkumar <vigneshrajkumar@hotmail.com>
2020-05-16 17:51:31 +0530

    added video chat window

--- client/src/components/ViewAreaFooter.js
+++ client/src/components/ViewAreaFooter.js
@@ -8,11 +8,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class ViewAreaFooter extends React.Component{
     render(){
+
+        if (this.props.inCallView){
+            return (
+                <div className="view-area-footer-in-call">
+                    <textarea placeholder="Enter your message" ></textarea>
+                    <button> <FontAwesomeIcon icon={faPaperPlane} /> Send </button>
+                    <button id="hang-button" onClick={() => {this.props.endVideoCall("x")}}> <FontAwesomeIcon icon={faVideo} /> Hang </button>
+                </div>
+            )
+        }
+
         return (
             <div className="view-area-footer">
                 <textarea placeholder="Enter your message" ></textarea>
                 <button> <FontAwesomeIcon icon={faPaperPlane} /> Send </button>
-                <button> <FontAwesomeIcon icon={faVideo} /> Video </button>
+                <button onClick={() => {this.props.startVideoCall("x")}}> <FontAwesomeIcon icon={faVideo} /> Video </button>
                 <button> <FontAwesomeIcon icon={faPhoneAlt} /> Audio </button>
             </div>
         )
