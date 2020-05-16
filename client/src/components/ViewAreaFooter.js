3a6b1de43ff4311708a82f64a717aa4c4d091a29
vigneshrajkumar <vigneshrajkumar@hotmail.com>
2020-05-16 11:27:41 +0530

    Ui updates; added icons

--- client/src/components/ViewAreaFooter.js
+++ client/src/components/ViewAreaFooter.js
@@ -1,13 +1,19 @@
 import React from "react"
 
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 class ViewAreaFooter extends React.Component{
     render(){
         return (
             <div className="view-area-footer">
-                <textarea ></textarea>
-                <button> Send </button>
-                <button> Video Call </button>
-                <button> Audio Call </button>
+                <textarea placeholder="Enter your message" ></textarea>
+                <button> <FontAwesomeIcon icon={faPaperPlane} /> Send </button>
+                <button> <FontAwesomeIcon icon={faVideo} /> Video </button>
+                <button> <FontAwesomeIcon icon={faPhoneAlt} /> Audio </button>
             </div>
         )
     }
